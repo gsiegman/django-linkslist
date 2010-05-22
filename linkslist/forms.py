6a657e9dad5541a848807c827571da0564aafc7e
Glenn Siegman <gsiegman@gsiegman.com>
2010-05-22 02:01:27 -0500

    removed ajax field dependency

--- linkslist/forms.py
+++ linkslist/forms.py
@@ -1,16 +1,7 @@
 from django import forms
 from django.conf import settings
 from linkslist.models import LinksListItem, LinksListItemImage
-from ajax_filtered_fields.forms import ForeignKeyByLetter
 
 class LinksListItemAdminForm(forms.ModelForm):
-    image = ForeignKeyByLetter(LinksListItemImage, field_name="name", required=False)
-    
     class Meta:
         model = LinksListItem
-
-    class Media:
-        js = (settings.ADMIN_MEDIA_PREFIX + "js/SelectBox.js",
-                 settings.ADMIN_MEDIA_PREFIX + "js/SelectFilter2.js",
-                 "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js",
-                 settings.STATIC_URL + "js/ajax_filtered_fields.js",)
